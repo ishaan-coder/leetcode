d0380908cb376a2b011aca491e188611295150a6
ishaan-coder <69079684+ishaan-coder@users.noreply.github.com>
2022-07-07 14:02:35 +0530

    Time: 29 ms (80.79%), Space: 24 MB (82.09%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -5,16 +5,16 @@ public:
         int i=0;
         vector<int>ans(n,0);
         ans[0]=nums[0];
-       for(i=1;i<n;i++)
-       {
+        for(i=1;i<n;i++)
+        {
             ans[i]=ans[i-1]*nums[i];
-       }
+        }
         ans[n-1]=ans[n-2];
         int product=nums[n-1];
         for(i=n-2;i>0;i--)
         {
             ans[i]=ans[i-1]*product;
-            product=product*nums[i];
+            product*=nums[i];
         }
         ans[0]=product;
         return ans;
