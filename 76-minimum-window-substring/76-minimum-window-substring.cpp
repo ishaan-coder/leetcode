e95bc14cdf3e83d58c9a5d3097bfed78906f7347
ishaan-coder <69079684+ishaan-coder@users.noreply.github.com>
2022-07-12 15:56:56 +0530

    Time: 20 ms (73.79%), Space: 7.8 MB (51.51%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -3,23 +3,19 @@ public:
     string minWindow(string s, string t) {
         int n=s.size();
         int m=t.size();
-        if(m>n)
-            return "";
-        int i=0,j=0;
+        int i=0;
+        int j=0;
+        int ans=0;
+        int len=INT_MAX;
         vector<int>freq(256,0);
         int count=0;
-        int ans=INT_MAX;
-        int x1=0;
-        int x2=0;
-        string temp="";
         for(i=0;i<m;i++)
-        {
-            if(freq[t[i]]==0)
-                count++;
+        {   if(freq[t[i]]==0)
+                 count++;
             freq[t[i]]++;
         }
-        i=0;
-        for(j=0;j<n;j++)
+                 i=0;
+        while(j<n)
         {
             freq[s[j]]--;
             if(freq[s[j]]==0)
@@ -28,26 +24,23 @@ public:
             {
                 while(count==0)
                 {
-                    int wind=j-i+1;
-                    if(ans>wind)
+                    if(len>j-i+1)
                     {
-                        ans=wind;
-                        x1=i;
-                        x2=j;
+                        len=j-i+1;
+                        ans=i;
                     }
                     freq[s[i]]++;
-                    
                     if(freq[s[i]]==1)
                         count++;
                     i++;
                 }
             }
+            j++;
         }
-        for(int k=x1;k<=x2;k++)
-            temp+=s[k];
-        if(ans!=INT_MAX)
-        return s.substr(x1,ans);
-        else
-            return "";
+                 if(len!=INT_MAX)
+                 return s.substr(ans,len);
+                 else
+                 return "";
+                    
     }
 };
