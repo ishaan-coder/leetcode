cfe303e0c4038714096c5dd82b94f0ee51a5927b
ishaan-coder <69079684+ishaan-coder@users.noreply.github.com>
2022-08-22 00:31:30 +0530

    Time: 5 ms (23.77%), Space: 7.8 MB (25.38%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int i=0;
-        int sum_even=0;
-        int sum_odd=0;
-        for(i=0;i<n;i++)
+        if(n==1)
+            return nums[0];
+        if(n==2)
+            return max(nums[0],nums[1]);
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<n;i++)
         {
-            if(i%2==0)
-                sum_even=max(sum_odd,sum_even+nums[i]);
-            else
-                sum_odd=max(sum_even,sum_odd+nums[i]);
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
         }
-        return max(sum_even,sum_odd);
+        return dp[n-1];
     }
 };
