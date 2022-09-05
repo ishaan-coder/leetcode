f7c4c2aaa0d5964c9ff3111f9c18b6c8f425ae65
ishaan-coder <69079684+ishaan-coder@users.noreply.github.com>
2022-09-06 01:04:34 +0530

    Time: 84 ms (15.29%), Space: 19.2 MB (8.14%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,55 +1,54 @@
 class Solution {
 public:
+    void bfs(vector<vector<int>>&vis,vector<vector<char>>&grid,int row,int col)
+    {
+        int n=grid.size();
+        int m=grid[0].size();
+        vis[row][col]=1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        while(!q.empty())
+        {
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int delr=-1;delr<=1;delr++)
+            {
+                for(int delc=-1;delc<=1;delc++)
+                {
+                     if((delr==-1&&delc==-1) || (delr==-1&&delc==1) || (delr==1&&delc==-1) || (delr==1&&delc==1)){
+                        continue;
+                    }
+                    int delrow=delr+row;
+                    int delcol=delc+col;
+                    if(delrow>=0 && delrow<n && delcol>=0 && delcol<m && !vis[delrow][delcol] && grid[delrow][delcol]=='1')
+                    {
+                         vis[delrow][delcol]=1;
+                        q.push({delrow,delcol});
+                       
+                    }
+                }
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
         int i,j;
-        int count=0;
-        queue<pair<int,int>>q;
-        int x,y;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int cnt=0;
         for(i=0;i<n;i++)
         {
             for(j=0;j<m;j++)
             {
                 
-                if(grid[i][j]=='1')
+                if(!vis[i][j] && grid[i][j]=='1')
                 {
-                     count++;
-                    q.push({i,j});
-                      grid[i][j]='0';
-                    while(!q.empty())
-                    {
-                        pair<int,int>temp=q.front();
-                        int x1=temp.first;
-                        int x2=temp.second;
-                        q.pop();
-                        if(x1+1<n && grid[x1+1][x2]=='1')
-                        {
-                         q.push({(x1+1),x2});   
-                              grid[x1+1][x2]='0';
-                        }
-                        if(x1-1>=0 && grid[x1-1][x2]=='1')
-                        {
-                            q.push({x1-1,x2});
-                              grid[x1-1][x2]='0';
-                        }
-                        if(x2+1<m && grid[x1][x2+1]=='1')
-                        {
-                            q.push({x1,x2+1});
-                              grid[x1][x2+1]='0';
-                        }
-                        if(x2-1>=0 && grid[x1][x2-1]=='1')
-                        {
-                            q.push({x1,x2-1});
-                              grid[x1][x2-1]='0';
-                        }
-                    }
-                    
-                   
+                   cnt++;
+                    bfs(vis,grid,i,j);
                 }
             }
         }
-        return count;
-        
+        return cnt;
     }
 };
