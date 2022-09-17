26c8352dbdca95d6122470e683f2df6281a3c2ff
ishaan-coder <69079684+ishaan-coder@users.noreply.github.com>
2022-09-17 12:28:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (91.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    int FindWays(int i,int j,int m,int n,vector<vector<int>>&memo)
-    {
-        if(i>=m || j>=n)
-            return 0;
-        if(i==m-1 && j==n-1)
-            return 1;
-        if(memo[i][j]!=-1)
-            return memo[i][j];
-        
-        else return memo[i][j]=FindWays(i,j+1,m,n,memo)+FindWays(i+1,j,m,n,memo);
-        
-    }
     int uniquePaths(int m, int n) {
-         vector<vector<int>>memo(m+1,vector<int>(n+1,-1));
-        int x=FindWays(0,0,m,n,memo);
-        if(m==1 && n==1)
-            return x;
-        
-        return memo[0][0];
+        int N=m+n-2;
+        int c=m-1;
+        double res=1;
+        for(int i=1;i<=c;i++)
+        {
+            res=res*(N-c+i)/i;
+        }
+        return (int)res;
     }
 };
