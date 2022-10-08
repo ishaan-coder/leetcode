1445c35811e2c18131e0d6fe312a9fbb79331fb2
ishaan-coder <69079684+ishaan-coder@users.noreply.github.com>
2022-10-08 14:46:36 +0530

    Time: 614 ms (5.08%), Space: 85.2 MB (28.80%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-       int left[n];
-        vector<int>right(n,0);
-        int width[n];
         stack<pair<int,int>>st;
+        stack<pair<int,int>>st2;
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        vector<int>width(n);
+        int ans=0; 
         for(int i=0;i<n;i++)
-        {   
-           
+        {
             if(st.size()==0)
             {
-                
                 left[i]=-1;
             }
-            else if(st.empty()==false && st.top().first<heights[i])
+            else if(st.size()>0 && heights[st.top().second]<heights[i])
             {
                 left[i]=st.top().second;
             }
-            else if(st.size()>0 && st.top().first>=heights[i])
+            else if(st.size()>0 && heights[st.top().second]>=heights[i])
             {
-                while(st.empty()==false && st.top().first>=heights[i])
+                while(st.size()>0 && heights[st.top().second]>=heights[i])
                 {
                     st.pop();
                 }
@@ -30,24 +30,21 @@ public:
                     left[i]=st.top().second;
             }
             st.push({heights[i],i});
+            
         }
-        
-       
-        stack<pair<int,int>>st2;
         for(int i=n-1;i>=0;i--)
         {
-            
             if(st2.size()==0)
             {
                 right[i]=n;
             }
-            else if(st2.empty()==false && st2.top().first<heights[i])
+            else if(st2.size()>0 && heights[st2.top().second]<heights[i])
             {
                 right[i]=st2.top().second;
             }
-           else if(st2.size()>0 && st2.top().first>=heights[i])
+            else if(st2.size()>0 && heights[st2.top().second]>=heights[i])
             {
-                while(st2.empty()==false && st2.top().first>=heights[i])
+                while(st2.size()>0 && heights[st2.top().second]>=heights[i])
                 {
                     st2.pop();
                 }
@@ -57,19 +54,17 @@ public:
                     right[i]=st2.top().second;
             }
             st2.push({heights[i],i});
-            // cout<<right[i];
+            
         }
         for(int i=0;i<n;i++)
-        {
-            // cout<<left[i]<<right[i]<<" ";
             width[i]=right[i]-left[i]-1;
-            // cout<<width[i];
-        }
-        int ans=0;
+        
         for(int i=0;i<n;i++)
         {
             ans=max(ans,width[i]*heights[i]);
         }
-            return ans;
+        return ans;
+        
+        
     }
 };
