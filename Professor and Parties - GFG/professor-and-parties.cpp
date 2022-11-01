9e0517c3d3cfde1b69a33327ac4ff9839c7e7c5a
ishaan-coder <69079684+ishaan-coder@users.noreply.github.com>
2022-11-01 13:38:42 +0530

    Added solution - LeetHub

---
+++ Professor and Parties - GFG/professor-and-parties.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    
+    string PartyType(long long int a[], int n)
+    {
+        set<int>st;
+        for(int i=0;i<n;i++)
+        {
+            if(st.find(a[i])!=st.end())
+            return "BOYS";
+            st.insert(a[i]);
+        }
+        return "GIRLS";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+
+        int n;
+        cin >> n;
+        long long int a[n+5];
+        for(int i =0;i<n;i++)
+            cin >> a[i];
+        Solution ob;
+        cout<< ob.PartyType(a, n) <<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
